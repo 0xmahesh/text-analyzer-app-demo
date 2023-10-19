1f74f21a521625b52f1e1db4b749a13f8497e9fb
Mahesh De Silva <mahesh.desilva@zuhlke.com>
2023-10-19 18:15:07 +0530

    Initial Commit

---
+++ BookReaderApp/BookReaderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookReaderAppApp.swift
+//  BookReaderApp
+//
+//  Created by Mahesh De Silva on 2023-10-19.
+//
+
+import SwiftUI
+
+@main
+struct BookReaderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
